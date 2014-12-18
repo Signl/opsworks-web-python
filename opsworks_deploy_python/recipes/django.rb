ed60e94c40b08bbce66ec7563b9e90efa649c97a
jasukkas <jasukkas.th@gmail.com>
2014-12-18 14:23:49 +0700

    Update execute to system

--- opsworks_deploy_python/recipes/django.rb
+++ opsworks_deploy_python/recipes/django.rb
@@ -30,7 +30,7 @@ node[:deploy].each do |application, deploy|
   if requirements
     Chef::Log.info("Installing using requirements file: #{requirements} with sudo")
     pip_cmd = ::File.join(deploy["venv"], 'bin', 'pip')
-    execute "sudo #{pip_cmd} install --source=#{Dir.tmpdir} -r #{::File.join(deploy[:deploy_to], 'current', requirements)}" do
+    system "sudo #{pip_cmd} install --source=#{Dir.tmpdir} -r #{::File.join(deploy[:deploy_to], 'current', requirements)}" do
       cwd ::File.join(deploy[:deploy_to], 'current')
       user deploy[:user]
       group deploy[:group]
