6fdffd44b60a53290a484181f62e78a7eb6f401a
jasukkas <jasukkas.th@gmail.com>
2015-03-03 16:03:12 +0700

    Fix bug#2

--- opsworks_deploy_python/recipes/django.rb
+++ opsworks_deploy_python/recipes/django.rb
@@ -55,28 +55,28 @@ node[:deploy].each do |application, deploy|
   end
   
   # Manually config celery file
-  execute "cp -f #{::File.join(deploy[:deploy_to], 'current', 'django-celery.conf')} /etc/supervisor.d/." do
-    cwd ::File.join(deploy[:deploy_to], 'current')
-    user 'root'
-    group deploy[:group]
-    environment 'HOME' => ::File.join(deploy[:deploy_to], 'shared')
-  end
+  #execute "cp -f #{::File.join(deploy[:deploy_to], 'current', 'django-celery.conf')} /etc/supervisor.d/." do
+  #  cwd ::File.join(deploy[:deploy_to], 'current')
+  #  user 'root'
+  #  group deploy[:group]
+  #  environment 'HOME' => ::File.join(deploy[:deploy_to], 'shared')
+  #end
   
   # Manually config gunicorn file
-  execute "cp -f #{::File.join(deploy[:deploy_to], 'current', 'gunicorn-config.py')} #{::File.join(deploy[:deploy_to], 'shared', '.')}" do
-    cwd ::File.join(deploy[:deploy_to], 'current')
-    user 'root'
-    group deploy[:group]
-    environment 'HOME' => ::File.join(deploy[:deploy_to], 'shared')
-  end
+  #execute "cp -f #{::File.join(deploy[:deploy_to], 'current', 'gunicorn-config.py')} #{::File.join(deploy[:deploy_to], 'shared', '.')}" do
+  #  cwd ::File.join(deploy[:deploy_to], 'current')
+  #  user 'root'
+  #  group deploy[:group]
+  #  environment 'HOME' => ::File.join(deploy[:deploy_to], 'shared')
+  #end
   
   # Reload supervisorctl
-  execute "supervisorctl reload" do
-    cwd ::File.join(deploy[:deploy_to], 'current')
-    user 'root'
-    group deploy[:group]
-    environment 'HOME' => ::File.join(deploy[:deploy_to], 'shared')
-  end
+  #execute "supervisorctl reload" do
+  #  cwd ::File.join(deploy[:deploy_to], 'current')
+  #  user 'root'
+  #  group deploy[:group]
+  #  environment 'HOME' => ::File.join(deploy[:deploy_to], 'shared')
+  #end
   
   # Migration
   if deploy["migrate"] && deploy["migration_command"]
