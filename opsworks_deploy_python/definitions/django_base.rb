dc1bb1cc56eba1f54569dd4b312eb6557e7ab09e
jasukkas <jasukkas.th@gmail.com>
2015-03-03 16:43:33 +0700

    Revert "Fix bug#3"
    This reverts commit daa0377c1660b4d49c538f74a9eb694c5259a98c.

--- opsworks_deploy_python/definitions/django_base.rb
+++ opsworks_deploy_python/definitions/django_base.rb
@@ -108,6 +108,13 @@ define :django_configure do
         subscribes :restart,  "gunicorn_config[#{gunicorn_command}]", :delayed
         subscribes :restart,  "template[#{django_cfg}]", :delayed
       end
+      
+      execute "supervisorctl reload" do
+        cwd ::File.join(deploy[:deploy_to], 'current')
+        user 'root'
+        group deploy[:group]
+        environment 'HOME' => ::File.join(deploy[:deploy_to], 'shared')
+      end
     end
     
     celery = Hash.new
@@ -126,18 +133,10 @@ define :django_configure do
         app_name application
       end
     end
-    
-    #if run_action
-    #  supervisor_service application do
-    #    action run_action
-    #  end
-    #end
-    
-    execute "supervisorctl reload" do
-      cwd ::File.join(deploy[:deploy_to], 'current')
-      user 'root'
-      group deploy[:group]
-      environment 'HOME' => ::File.join(deploy[:deploy_to], 'shared')
+    if run_action
+      supervisor_service application do
+        action run_action
+      end
     end
   end
 end
