79a11dca788fcfcdbc9a7c740a810ade29dec9f9
jasukkas <jasukkas.th@gmail.com>
2015-05-13 15:26:25 +0700

    Adding more apt-get

--- opsworks_deploy_python/recipes/django.rb
+++ opsworks_deploy_python/recipes/django.rb
@@ -32,6 +32,8 @@ node[:deploy].each do |application, deploy|
   system "sudo apt-get -y install redis-server"
   system "sudo apt-get -y install gunicorn"
   system "sudo apt-get -y install libevent-dev"
+  system "sudo apt-get -y install libffi-dev"
+  system "sudo apt-get -y install libssl-dev"
   
   # Install requirements
   requirements = Helpers.django_setting(deploy, 'requirements', node)
